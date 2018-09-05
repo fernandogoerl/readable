343ab7119816e879f85a3db6cb09f689dee05643
Fernando Goerl <fernandogoerl@gmail.com>
2018-09-05 12:38:17 -0300

    feat: redux now manages state

--- frontend/src/containers/App.js
+++ frontend/src/containers/App.js
@@ -1,34 +1,16 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import Home from './Home';
-import Category from './Category';
-import * as api from '../api/api';
 
 
 export default class App extends Component {
-    state = {
-        categories: [],
-        posts: [],
-    }
-
-    componentDidMount () {
-        api.getAllCategories().then((categories) => {
-            this.setState({ categories });
-        });
-        api.getAllPosts().then((posts) => {
-            this.setState({ posts });
-        });
-    };
 
     render() {
         return (
             <div>
-                <Route exact path='/' render={() => (
-                    <Home state={this.state} />
-                )} />
-                <Route path='/:category' render={() => (
-                    <Category state={this.state} />
-                )} />
+                <Route exact path='/' component={Home} />
+                <Route exact path='/:category' component={Home} />
+
             </div>
         );
     }
