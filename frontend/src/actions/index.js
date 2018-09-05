39afe961caab90d73b15669dcb1379d8fd7823cd
Fernando Goerl <fernandogoerl@gmail.com>
2018-09-05 12:34:57 -0300

    feat: new actions

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -1,11 +1,29 @@
-export const LOAD_POSTS_SUCCESS = 'LOAD_POSTS_SUCCESS';
-export const LOAD_CATEGORIES_SUCCESS = 'LOAD_CATEGORIES_SUCCESS';
+import * as api from '../api'
 
-// export const getCategories = () => {
-//     return {
-//         type: ADD_RECIPE,
-//         recipe,
-//         day,
-//         meal,
-//     }
-// }
+export const LOAD_CATEGORIES = 'LOAD_CATEGORIES';
+export const LOAD_POSTS = 'LOAD_POSTS';
+
+
+export const loadCategories = (categories) => {
+    return {
+        type: LOAD_CATEGORIES,
+        categories
+    };
+}
+
+export const fetchCategories = () => (dispatch) =>
+    api.getAllCategories()
+        .then((categories) => dispatch(loadCategories(categories)))
+
+
+export const loadAllPosts = (posts) => {
+    return {
+        type: LOAD_POSTS,
+        posts
+    };
+}
+
+export const fetchAllPosts = () => (dispatch) => {
+    api.getAllPosts()
+        .then((posts) => dispatch(loadAllPosts(posts)));
+}
