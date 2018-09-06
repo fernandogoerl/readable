572d2ec48dca53432d090f34efe98b5523bbfa80
Fernando Goerl <fernandogoerl@gmail.com>
2018-09-05 22:19:22 -0300

    feat: new component footer

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './Footer.css'
+
+class Footer extends Component {
+    render() {
+        return (
+            <div className='footer'>
+                Here is the footer! by Fernando Goerl
+            </div>
+        );
+    }
+}
+
+export default Footer;
