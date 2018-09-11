16639a5b3a2a15d075239fd02fc7d1e3b9d954bd
Fernando Goerl <fernandogoerl@gmail.com>
2018-09-11 19:04:45 -0300

    refactor: improved default style

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 
 import './index.css';
+
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 
