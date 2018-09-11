715b885577bbba2f663298cbc950362bed6a072f
Fernando Goerl <fernandogoerl@gmail.com>
2018-09-11 19:02:51 -0300

    feat: import Header and Footer components

--- frontend/src/containers/App.js
+++ frontend/src/containers/App.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import Home from './Home';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import Post from '../components/Post';
 
 
 export default class App extends Component {
 
+
     render() {
         return (
             <div>
+                <Header/>
                 <Route exact path='/' component={Home} />
                 <Route exact path='/:category' component={Home} />
-
+                <Route exact path='/post/:id' component={Post} />
+                <Footer/>
             </div>
         );
     }
