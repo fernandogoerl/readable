a7ceecd1aabe50d86d91f5b0ee6d348abd55e6f0
Fernando Goerl <fernandogoerl@gmail.com>
2018-09-13 19:44:45 -0300

    feat: new timeConverter helper

--- frontend/src/helpers/index.js
+++ frontend/src/helpers/index.js
@@ -2,7 +2,7 @@ const compare = (post1, post2, orderBy) => {
     switch(orderBy) {
         case 'author' :
         case 'title' :
-        case 'timeStamp' :
+        case 'timestamp' :
             if(post1[orderBy] < post2[orderBy]) { return -1; }
             else if(post1[orderBy] > post2[orderBy]) { return 1; }
             else { return post2.voteScore - post1.voteScore; }
@@ -17,3 +17,5 @@ const parseOrder = (str) => (str.substring(7));
 export const order = (posts, orderBy) =>
     (posts.sort((post1, post2) =>
         compare(post1, post2, parseOrder(orderBy))));
+
+export const timeConverter = (timestamp) => (new Date(timestamp));
